70973d317f8c7accea173723c58ca7725dfea1b4
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-17 16:47:12 +0300

    -UserController , user entity with endpoint;

---
+++ ws/src/main/java/com/socialization/ws/user/User.java
@@ -0,0 +1,10 @@
+package com.socialization.ws.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String displayName;
+    private String password;
+}
