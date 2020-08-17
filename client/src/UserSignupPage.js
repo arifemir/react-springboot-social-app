2e1e6dfba5a8c34e25d4f628986fe895efca580c
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-17 16:08:47 +0300

    - user signup page state handling done;

--- client/src/UserSignupPage.js
+++ client/src/UserSignupPage.js
@@ -1,25 +1,38 @@
 import React, {Component} from 'react';
 
 class UserSignupPage extends Component {
+
+    state = {
+        userName: null,
+        displayName: null,
+        password: null,
+        passwordRepeat: null
+    }
+
+    onChangeFields = event => {
+        const {value, name} = event.target
+        this.setState({[name]: value});
+    }
+
     render() {
         return (
             <form>
                 <h1>Sign Up</h1>
                 <div>
-                    <label htmlFor="">Username</label>
-                    <input type="text"/>
+                    <label htmlFor=''>Username</label>
+                    <input name='userName' onChange={this.onChangeFields} type='text'/>
                 </div>
                 <div>
-                    <label htmlFor="">Display Name</label>
-                    <input type="text"/>
+                    <label htmlFor=''>Display Name</label>
+                    <input name='displayName' onChange={this.onChangeFields} type='text'/>
                 </div>
                 <div>
-                    <label htmlFor="">Password</label>
-                    <input type="password"/>
+                    <label htmlFor=''>Password</label>
+                    <input name='password' onChange={this.onChangeFields} type='password'/>
                 </div>
                 <div>
                     <label htmlFor="">Password Repeat</label>
-                    <input type="password"/>
+                    <input name='passwordRepeat' onChange={this.onChangeFields} type='password'/>
                 </div>
                 <button>Sign Up</button>
             </form>
