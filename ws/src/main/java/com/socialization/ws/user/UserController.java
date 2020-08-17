f10c6e48e215a132578b7332f74ff497409b9d69
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-17 17:52:18 +0300

    -user storing h2db UserRepository makes db processes;

--- ws/src/main/java/com/socialization/ws/user/UserController.java
+++ ws/src/main/java/com/socialization/ws/user/UserController.java
@@ -2,6 +2,7 @@ package com.socialization.ws.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,12 @@ public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping("/api/1.0/users")
     public void createUser(@RequestBody User user) {
         log.info(user.toString());
+        userRepository.save(user);
     }
 }
