ef7ca610f1591121cf163ab25d01e191acbdad9d
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-17 18:13:58 +0300

    -UserService for db processes layered system; -GenericResponse response endpoints;

---
+++ ws/src/main/java/com/socialization/ws/user/UserService.java
@@ -0,0 +1,17 @@
+package com.socialization.ws.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+}
