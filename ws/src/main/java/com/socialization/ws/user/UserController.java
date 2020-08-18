4b1cf75977613be8cc4541f76492f6603df654c5
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-18 03:43:48 +0300

    -username data validation backend;

--- ws/src/main/java/com/socialization/ws/user/UserController.java
+++ ws/src/main/java/com/socialization/ws/user/UserController.java
@@ -1,14 +1,19 @@
 package com.socialization.ws.user;
 
+import com.socialization.ws.Error.ApiError;
 import com.socialization.ws.Shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -24,8 +29,16 @@ public class UserController {
 
     @PostMapping("/api/1.0/users")
     @ResponseStatus(HttpStatus.CREATED)
-    public GenericResponse createUser(@RequestBody User user) {
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        String username = user.getUserName();
+        if(username == null || username.isEmpty()) {
+            ApiError error = new ApiError(400, "Validation error", "api/1.0/users");
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username", "Username cannot be null");
+            error.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
         userService.save(user);
-        return new GenericResponse("user created");
+        return ResponseEntity.ok(new GenericResponse("user created"));
     }
 }
