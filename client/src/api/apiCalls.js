6eabb6cc2c0be435a849765a2bf6e3f5c2084cb6
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-19 19:17:04 +0300

    -i18n for backend;

--- client/src/api/apiCalls.js
+++ client/src/api/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const signup = (body) => {
-  return axios.post('/api/1.0/users', body)
+  return axios.post('/api/1.0/users', body, {headers: {'accept-language':'en-us'}})
 }
 
 export {
