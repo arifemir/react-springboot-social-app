7c0b0b51dfcfabd021e340c348cd59654e8f0d55
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-19 19:43:59 +0300

    -client i18 react;

--- client/src/pages/UserSignupPage.js
+++ client/src/pages/UserSignupPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {signup} from '../api/apiCalls';
 import Input from '../components/input';
+import {withTranslation} from 'react-i18next'
 
 class UserSignupPage extends Component {
 
@@ -14,15 +15,16 @@ class UserSignupPage extends Component {
     }
 
     onChangeFields = event => {
+        const {t} = this.props;
         const {value, name} = event.target
         const errors = {...this.state.errors}
         errors[name] = undefined
 
         if (name === 'password' || name === 'passwordRepeat') {
             if (name === 'password' && value !== this.state.passwordRepeat) {
-                errors.passwordRepeat = 'Password mismatch'
+                errors.passwordRepeat = t('Password mismatch')
             } else if (name === 'passwordRepeat' && value !== this.state.password) {
-                errors.passwordRepeat = 'Password mismatch'
+                errors.passwordRepeat = t('Password mismatch')
             } else {
                 errors.passwordRepeat = undefined
             }
@@ -48,22 +50,24 @@ class UserSignupPage extends Component {
     }
 
     render() {
+        const {t} = this.props
         const {pendingApiCall, errors} = this.state
         const {userName, displayName, password, passwordRepeat} = errors
+
         return (
             <div className='container'>
                 <form>
-                    <h1 className='text-center'>Sign Up</h1>
-                    <Input label='Username' name='userName' onChangeFields={this.onChangeFields} error={userName} type='text'/>
-                    <Input label='Display Name' name='displayName' onChangeFields={this.onChangeFields} error={displayName} type='text'/>
-                    <Input label='Password' name='password' onChangeFields={this.onChangeFields} error={password} type='password' />
-                    <Input label='Password Repeat' name='passwordRepeat' onChangeFields={this.onChangeFields} error={passwordRepeat} type='password' />
+                    <h1 className='text-center'>{t('Sign Up')}</h1>
+                    <Input label={t('Username')} name='userName' onChangeFields={this.onChangeFields} error={userName} type='text'/>
+                    <Input label={t('Display name')} name='displayName' onChangeFields={this.onChangeFields} error={displayName} type='text'/>
+                    <Input label={t('Password')} name='password' onChangeFields={this.onChangeFields} error={password} type='password' />
+                    <Input label={t('Password Repeat')} name='passwordRepeat' onChangeFields={this.onChangeFields} error={passwordRepeat} type='password' />
                     
                     <div className='text-center'>
                         <button disabled={pendingApiCall || passwordRepeat !== undefined} className='btn btn-primary' onClick={this.onClickSignUp} >
                         {pendingApiCall ? 
                             <span className='spinner-border spinner-border-sm'/> :
-                            <span>Sign Up</span>
+                            <span>{t('Sign Up')}</span>
                         } 
                         </button>
                     </div>
@@ -73,4 +77,6 @@ class UserSignupPage extends Component {
     }
 }
 
-export default UserSignupPage;
+
+
+export default withTranslation()(UserSignupPage);
